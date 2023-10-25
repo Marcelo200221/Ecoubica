64b6ec56c8885b444eb742d8d8250e8ba1d3bf9d
RaulFVH <rau.valenzuela@duocuc.cl>
2023-10-24 22:46:15 -0300

    Mapa listo!!!
    Mapa agregado con sus distintos puntos habilitados, deshabilitados y en mantención.

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -56,33 +56,237 @@
     </div>
 </header>
 
+<h1 class="text-center mt-4">Mapa de Ubicaciones Ecoubica</h1>
+    <div id="map"></div>
+
 <div id="map"></div>
-<img id="image" src="img/re.svg" style="display: none;">
 
 <script>
+    var map;
+    var image;
+    var marker1, marker2, marker3, marker4, marker5, marker6, marker7, marker8, marker9, marker10;
+
     function initMap() {
-        var ubicacion = {lat: -33.45694, lng: -70.64827};
-        var map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
             zoom: 12,
-            center: ubicacion
+            center: {lat: -33.45694, lng: -70.64827},
+            styles: [
+                {
+                    featureType: "poi",
+                    elementType: "labels",
+                    stylers: [{ visibility: "off" }]
+                },
+                {
+                    featureType: "transit",
+                    elementType: "labels",
+                    stylers: [{ visibility: "off" }]
+                }
+            ]
         });
 
-        var image = document.getElementById('image');
+        var ubicacion1 = {lat: -33.45694, lng: -70.64827};
+        var ubicacion2 = {lat: -33.5227, lng: -70.6007};
+        var ubicacion3 = {lat: -33.4500, lng: -70.6336};
+        var ubicacion4 = {lat: -33.4939, lng: -70.6246};
+        var ubicacion5 = {lat: -33.5209, lng: -70.7519};
+        var ubicacion6 = {lat: -33.5924, lng: -70.6274};
+        var ubicacion7 = {lat: -33.4575, lng: -70.6783};
+        var ubicacion8 = {lat: -33.4929, lng: -70.5990};
+        var ubicacion9 = {lat: -33.4166, lng: -70.5927};
+        var ubicacion10 = {lat: -33.5686, lng: -70.6761};
 
-        var iconoPersonalizado = {
-            url: image.src, 
-            size: new google.maps.Size(30, 30), 
-            scaledSize: new google.maps.Size(30, 30) 
-        };
+        var infoWindow1 = new google.maps.InfoWindow({
+            content: 'Punto Verde Deshabilitado'
+        });
 
-        var marker = new google.maps.Marker({
-            position: ubicacion,
+        var infoWindow2 = new google.maps.InfoWindow({
+            content: 'Punto Verde en Mantención'
+        });
+
+        var infoWindow3 = new google.maps.InfoWindow({
+            content: 'Punto Verde Habilitado'
+        });
+
+        var infoWindow4 = new google.maps.InfoWindow({
+            content: 'Punto Verde en Mantención'
+        });
+
+        var infoWindow5 = new google.maps.InfoWindow({
+            content: 'Punto Verde Deshabilitado'
+        });
+
+        var infoWindow6 = new google.maps.InfoWindow({
+            content: 'Punto Verde Habilitado'
+        });
+
+        var infoWindow7 = new google.maps.InfoWindow({
+            content: 'Punto Verde Habilitado'
+        });
+
+        var infoWindow8 = new google.maps.InfoWindow({
+            content: 'Punto Verde Habilitado'
+        });
+
+        var infoWindow9 = new google.maps.InfoWindow({
+            content: 'Punto Verde en Mantención'
+        });
+
+        var infoWindow10 = new google.maps.InfoWindow({
+            content: 'Punto Verde Deshabilitado'
+        });
+
+        marker1 = new google.maps.Marker({
+            position: ubicacion1,
             map: map,
-            title: 'Ubicación',
-            icon: iconoPersonalizado
+            title: 'Ubicación 1',
+            icon: {
+                url: 'img/C.rojo.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker2 = new google.maps.Marker({
+            position: ubicacion2,
+            map: map,
+            title: 'Ubicación 2',
+            icon: {
+                url: 'img/C.amarillo.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker3 = new google.maps.Marker({
+            position: ubicacion3,
+            map: map,
+            title: 'Ubicación 3',
+            icon: {
+                url: 'img/C.verde.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker4 = new google.maps.Marker({
+            position: ubicacion4,
+            map: map,
+            title: 'Ubicación 4',
+            icon: {
+                url: 'img/C.amarillo.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker5 = new google.maps.Marker({
+            position: ubicacion5,
+            map: map,
+            title: 'Ubicación 5',
+            icon: {
+                url: 'img/C.rojo.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker6 = new google.maps.Marker({
+            position: ubicacion6,
+            map: map,
+            title: 'Ubicación 6',
+            icon: {
+                url: 'img/C.verde.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker7 = new google.maps.Marker({
+            position: ubicacion7,
+            map: map,
+            title: 'Ubicación 7',
+            icon: {
+                url: 'img/C.verde.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker8 = new google.maps.Marker({
+            position: ubicacion8,
+            map: map,
+            title: 'Ubicación 8',
+            icon: {
+                url: 'img/C.verde.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker9 = new google.maps.Marker({
+            position: ubicacion9,
+            map: map,
+            title: 'Ubicación 9',
+            icon: {
+                url: 'img/C.amarillo.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker10 = new google.maps.Marker({
+            position: ubicacion10,
+            map: map,
+            title: 'Ubicación 10',
+            icon: {
+                url: 'img/C.rojo.png',  
+                size: new google.maps.Size(30, 30),  
+                scaledSize: new google.maps.Size(30, 30) 
+            }
+        });
+
+        marker1.addListener('click', function() {
+            infoWindow1.open(map, marker1);
+        });
+
+        marker2.addListener('click', function() {
+            infoWindow2.open(map, marker2);
+        });
+
+        marker3.addListener('click', function() {
+            infoWindow3.open(map, marker3);
+        });
+
+        marker4.addListener('click', function() {
+            infoWindow4.open(map, marker4);
+        });
+
+        marker5.addListener('click', function() {
+            infoWindow5.open(map, marker5);
+        });
+
+        marker6.addListener('click', function() {
+            infoWindow6.open(map, marker6);
+        });
+
+        marker7.addListener('click', function() {
+            infoWindow7.open(map, marker7);
+        });
+
+        marker8.addListener('click', function() {
+            infoWindow8.open(map, marker8);
+        });
+
+        marker9.addListener('click', function() {
+            infoWindow9.open(map, marker9);
+        });
+
+        marker10.addListener('click', function() {
+            infoWindow10.open(map, marker10);
         });
     }
 </script>
+
 
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBmzxktL-ApQe9WO-ZSSk-CtypQuN_QwEc&callback=initMap"></script>
 
